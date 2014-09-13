f0a4b759f67e510ff749092360d7c7c73b393f29
Johannes Steudle <external.johannes.steudle@bosch-softtec.com>
2014-09-14 00:35:16 +0200

    First working version

--- OpenPhoto/OPViewController.h
+++ OpenPhoto/OPViewController.h
@@ -8,6 +8,22 @@
 
 #import <UIKit/UIKit.h>
 
-@interface OPViewController : UIViewController
+@interface OPViewController : UIViewController <UIPickerViewDataSource,UIPickerViewDelegate>
+
+@property NSNumber* z;
+@property (strong, nonatomic) IBOutlet UIPickerView *focalLengthsPicker;
+@property (strong, nonatomic) IBOutlet UIPickerView *dataPicker;
+
+@property (weak, nonatomic) IBOutlet UITextField *focalLengthTextField;
+@property (weak, nonatomic) IBOutlet UITextField *distance;
+@property (weak, nonatomic) IBOutlet UITextField *aperture;
+@property (weak, nonatomic) IBOutlet UITextField *result;
+@property (weak, nonatomic) IBOutlet UIButton *calculate;
+
+@property (strong, nonatomic) NSArray* focalLengths;
+@property (strong, nonatomic) NSArray* apertureData;
+@property (strong, nonatomic) NSMutableArray* distanceData;
+
+- (IBAction) calculate:(id)sender;
 
 @end
--- OpenPhoto/OPViewController.m
+++ OpenPhoto/OPViewController.m
@@ -14,16 +14,102 @@
 
 @implementation OPViewController
 
+@synthesize z;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.dataPicker.delegate = self;
+    self.dataPicker.dataSource = self;
+    self.focalLengths  = [[NSArray alloc] initWithObjects:@"24 mm", @"35 mm", @"50 mm", @"60 mm", @"70 mm", nil];
+    self.apertureData = [[NSArray alloc] initWithObjects:@"1.4", @"1.8", @"2.0", @"2.8",@"4.0",@"5.6",@"6.3",@"8.0",@"16", @"18", @"20", @"22", nil];
+    
+    self.distanceData = [[NSMutableArray alloc] init];
+    
+    for (int i = 0; i < 100; i++)
+    {
+        [self.distanceData addObject:[NSString stringWithFormat:@"%d m",i+1]];
+    }
+    
+    self.z = [[NSNumber alloc] initWithDouble:0.03];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)calculate:(id)sender
+{
+    NSLog(@"calculate");
+    _result.text = _focalLengthTextField.text;
+
+}
+
+-(IBAction)textFieldReturn:(id)sender
+{
+    [sender resignFirstResponder];
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 3;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent: (NSInteger)component
+{
+    if (component == 0)
+    {
+        return [self.focalLengths count];
+    }
+    else if (component == 1)
+    {
+        return [self.apertureData count];
+    }
+    else
+    {
+        return [self.distanceData count];
+    }
+}
+
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row   forComponent:(NSInteger)component
+{
+    if (component == 0)
+    {
+        return [self.focalLengths objectAtIndex:row];
+    }
+    else if (component == 1)
+    {
+        return [self.apertureData objectAtIndex:row];
+    }
+    else
+    {
+        return [self.distanceData objectAtIndex:row];
+    }
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row   inComponent:(NSInteger)component
+{
+    NSInteger focalLengthIndex = [pickerView selectedRowInComponent:0];
+    NSInteger apertureValueIndex = [pickerView selectedRowInComponent:1];
+    NSInteger distanceIndex = [pickerView selectedRowInComponent:2];
+    
+    NSNumber* focalLength = [[NSNumber alloc] initWithDouble:[[[self.focalLengths objectAtIndex:focalLengthIndex] substringWithRange:NSMakeRange(0, 2)] doubleValue]];
+    NSNumber* apertureValue = [[NSNumber alloc] initWithDouble:[[self.apertureData objectAtIndex:apertureValueIndex] doubleValue]];
+    
+    
+    self.result.text = [[self calculateHyperFocal:focalLength withApertureValue:apertureValue] stringValue];
+}
+
+- (NSNumber*) calculateHyperFocal:(NSNumber*) focalLength withApertureValue:(NSNumber*) apertureValue
+{
+    NSNumber* result = [[NSNumber alloc] initWithDouble:(([focalLength doubleValue] * [focalLength doubleValue])/([apertureValue doubleValue] * [self.z doubleValue]) + [focalLength doubleValue])/1000];
+
+    NSLog(@"Using parameter focalLength: %@", focalLength);
+    NSLog(@"Using parameter aperture: %@", apertureValue);
+    NSLog(@"Calculating hyperfocal distance: %@", result);
+    return result;
 }
 
 @end
